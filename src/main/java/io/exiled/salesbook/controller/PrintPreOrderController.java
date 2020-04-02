80b574b2fa237135f94def44f487c5e20492ab00
Exiled <gett_lostt@bk.ru>
2020-04-02 18:59:45 +0200

    Stuck

--- src/main/java/io/exiled/salesbook/controller/PrintPreOrderController.java
+++ src/main/java/io/exiled/salesbook/controller/PrintPreOrderController.java
@@ -1,41 +1,41 @@
-//package io.exiled.salesbook.controller;
-//
-//import io.exiled.salesbook.model.PrintPreOrder;
-//import io.exiled.salesbook.service.AlpService;
-//import io.exiled.salesbook.service.LinkerService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-////@EnableWebMvc
-//@Controller
-//public class PrintPreOrderController {
-//
-//    private AlpService alpService;
-//    private LinkerService service;
-//
-//    private String uploadPath = "/home/exile/dev/Photory/upd";
-//    private String defaultFolder = "/home/exile/dev/Photory/Linker/Tallinn Trophy";
-//
-//    @Autowired
-//    public PrintPreOrderController(AlpService alpService, LinkerService service) {
-//        this.alpService = alpService;
-//        this.service = service;
-//    }
-//
-//    @RequestMapping(path = "/preorder/{linkA}")
-//    public String getFolders(
-//            @PathVariable("linkA") String linkA,
-//            Model model
-//    ) {
-//        String updatedLink = defaultFolder + "/" + linkA;
-//        model.addAttribute("folders", service.buildLinks(updatedLink));
-//        alpService.refresh();
-//        alpService.getPaths();
-//        alpService.makeCollection();
-//        model.addAttribute("alps", alpService.getAlps());
-//        return "linker";
-//    }
-//}
+package io.exiled.salesbook.controller;
+
+import io.exiled.salesbook.model.PrintPreOrder;
+import io.exiled.salesbook.service.AlpService;
+import io.exiled.salesbook.service.LinkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//@EnableWebMvc
+@Controller
+public class PrintPreOrderController {
+
+    private AlpService alpService;
+    private LinkerService service;
+
+    private String uploadPath = "/home/exile/dev/Photory/upd";
+    private String defaultFolder = "/home/exile/Desktop/Linker/FigureSkating";
+
+    @Autowired
+    public PrintPreOrderController(AlpService alpService, LinkerService service) {
+        this.alpService = alpService;
+        this.service = service;
+    }
+
+    @RequestMapping(path = "/preorder/{linkA}")
+    public String getFolders(
+            @PathVariable("linkA") String linkA,
+            Model model
+    ) {
+        String updatedLink = defaultFolder + "/" + linkA;
+        model.addAttribute("folders", service.buildLinks(updatedLink));
+        alpService.refresh();
+        alpService.getPaths();
+        alpService.makeCollection();
+        model.addAttribute("alps", alpService.getAlps());
+        return "linker";
+    }
+}
--- src/main/java/io/exiled/salesbook/service/impl/LinkerServiceImpl.java
+++ src/main/java/io/exiled/salesbook/service/impl/LinkerServiceImpl.java
@@ -4,98 +4,85 @@ import io.exiled.salesbook.model.Linker;
 import io.exiled.salesbook.service.LinkerService;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-//
-//import io.exiled.salesbook.model.Linker;
-//import io.exiled.salesbook.service.LinkerService;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.File;
-//import java.util.ArrayList;
-//import java.util.List;
-//
 @Service
 public class LinkerServiceImpl implements LinkerService {
-    @Override
-    public List<Linker> buildLinks(String link) {
-        List<Linker> ll = new ArrayList<>();
-        ll.add(new Linker("aa","bb"));
-        ll.add(new Linker("cc","dd"));
-        return ll;
-    }
-}
-//    //    @Value("${photory.path}")
-//    private String rootPath = "/home/exile/dev/Photory/Linker/Tallinn Trophy";
-//
-//    private String fullPath = "/home/exile/dev/Photory/Linker/Tallinn Trophy";
-//
-////    private StringBuilder fullLink = new StringBuilder("").append(rootPath);
-////    private StringBuilder shortPath = new StringBuilder("");
-//
-//    public List<Linker> buildLinks(String root) {
-//        List<Linker> links = new ArrayList<>();
-////        if (root.endsWith(".css")) {
-////            root = "";
-////        }
-////        String updatedLink = fullPath + createLink(root);
-//        String updatedLink = fullPath + root;
-////        try {
-////            if (!root.isEmpty()) {
-//////                Files.list(Paths.get(updatedLink))
-//////                        .filter(Files::isDirectory)
-//////                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
-////                Files.list(Paths.get(updatedLink))
-////                        .filter(Files::isDirectory)
-////                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
-////            } else {
-////                Files.list(Paths.get(rootPath))
-////                        .filter(Files::isDirectory)
-////                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
-////            }
-////        }
-//        try {
-////            File fileName = new File(updatedLink);
-////            File[] fileList = fileName.listFiles();
-////
-////            for (File file : fileList) {
-//////                new Linker(file.toString().substring(fullPath.length() + 1), file.toString().substring(fullPath.length() + 1));
-////                new Linker("link A", "link B");
-////            }
-//        } catch (Exception e) {
-//            System.err.println("Error in BuildLinks Method " + e.getMessage());
+    //    @Value("${photory.path}")
+    private String rootPath = "/home/exile/dev/Photory/Linker/Tallinn Trophy";
+
+    private String fullPath = "/home/exile/dev/Photory/Linker/Tallinn Trophy";
+
+//    private StringBuilder fullLink = new StringBuilder("").append(rootPath);
+//    private StringBuilder shortPath = new StringBuilder("");
+
+    public List<Linker> buildLinks(String root) {
+        List<Linker> links = new ArrayList<>();
+//        if (root.endsWith(".css")) {
+//            root = "";
 //        }
-//        return links;
+//        String updatedLink = fullPath + createLink(root);
+        String updatedLink = fullPath + root;
+        try {
+            if (!root.isEmpty()) {
+//                Files.list(Paths.get(updatedLink))
+//                        .filter(Files::isDirectory)
+//                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
+                Files.list(Paths.get(updatedLink))
+                        .filter(Files::isDirectory)
+                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
+            } else {
+                Files.list(Paths.get(rootPath))
+                        .filter(Files::isDirectory)
+                        .forEach(f -> links.add(new Linker(f.toString().substring(fullPath.length() + 1), f.toString().substring(fullPath.length() + 1))));
+            }
+        }
+//        try {
+//            File fileName = new File(updatedLink);
+//            File[] fileList = fileName.listFiles();
+//
+//            for (File file : fileList) {
+////                new Linker(file.toString().substring(fullPath.length() + 1), file.toString().substring(fullPath.length() + 1));
+//                new Linker("link A", "link B");
+//            }
+//        }
+        catch (Exception e) {
+            System.err.println("Error in BuildLinks Method " + e.getMessage());
+        }
+        return links;
+    }
+
+//    @Deprecated
+//    private String createLink(String addition) {
+//        StringBuilder createdLink = new StringBuilder("").append(fullLink);
+//        if (shortPath.toString().contains(addition)) {
+//            createdLink.append(shortPath.substring(0, shortPath.indexOf(addition) + addition.length()));
+//        } else {
+//            createdLink.append(shortPath.append("/").append(addition));
+//        }
+////        this.fullPath = fullPath + shortPath;
+//        return createdLink.toString();
 //    }
-//
-////    @Deprecated
-////    private String createLink(String addition) {
-////        StringBuilder createdLink = new StringBuilder("").append(fullLink);
-////        if (shortPath.toString().contains(addition)) {
-////            createdLink.append(shortPath.substring(0, shortPath.indexOf(addition) + addition.length()));
-////        } else {
-////            createdLink.append(shortPath.append("/").append(addition));
-////        }
-//////        this.fullPath = fullPath + shortPath;
-////        return createdLink.toString();
-////    }
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
